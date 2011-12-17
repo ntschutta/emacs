a86bc2575a499f7484f5aa171fe9ff660c5dedb5
Nate Schutta <ntschutta@gmail.com>
2011-12-17 17:16:01 -0600

    added a couple of new keybings

--- init.el
+++ init.el
@@ -252,3 +252,10 @@
 (custom-set-variables
  '(speedbar-show-unknown-files t)
 )
+
+;; modify some keybings...
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-c\C-k" 'kill-region)
